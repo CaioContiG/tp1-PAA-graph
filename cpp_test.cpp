8a9f23d0b0424007e45370d8b6b13dd7405bebd7
unknown <caiocontig@gmail.com>
2024-05-04 10:19:59 -0300

    readSample added

--- cpp_test.cpp
+++ cpp_test.cpp
@@ -1,6 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std; 
 
+// Node as words or edges as words? Let's try edges
+// Adjacency Matrix
+
+
+// Extract values from sample string
+void readSample(string input){
+
+    // Initiate variables
+    int q_words = -1;
+    string from;
+    string to;
+    string l1;
+    string l2;
+    string p;
+
+    // Using stringstream to collect values
+    stringstream ss(input);
+    
+    while(true){        
+        ss >> q_words; // Pick the number of words expected
+        if (q_words == 0){break; } // Stop if no words are being expected
+        ss >> from; // Pick the language we want to begin with
+        ss >> to; // Pick te language we want to reach
+        cout << q_words << " " << from << " " << to << endl;
+
+        // Iterates over the string to grab the words
+        for (int i = 0; i < q_words; i++ ){
+            for (int j = 0; j < 3; j++){
+                if (j == 0){ss >> l1;};
+                if (j == 1){ss >> l2;};
+                if (j == 2){ss >> p;};
+            }
+            cout << l1 << " " << l2 << " " << p << endl;
+        }
+        cout << "-------------" << endl;
+    }
+ 
+}
+
 string mySample(){
     // First Line, number of words
     // Second, start and finish language
@@ -35,8 +74,9 @@ int main() {
     string v_input;
     //cin >> v_input;
     //cout << v_input;
-    string a;
-    a = mySample();
-    cout << a;
+    string sample;
+    sample = mySample();
+    readSample(sample);   
+
     return 0;
 }
