51b6ec2341a4de1a96389557582de30532b5e701
unknown <caiocontig@gmail.com>
2024-05-16 18:34:31 -0300

    graph update

--- cpp_test.cpp
+++ cpp_test.cpp
@@ -5,15 +5,18 @@ using namespace std;
 class Node{
     public:
         string word;
+        string language;
         string language1;
         string language2;
+        int distance;
         int idx;
         int length;
         int idx_language1;
         int idx_language2;
         // Construtor
-        Node(string l1,string l2,string p, int i){
+        Node(string l1,string l2,string p, int i, string lang){
             word = p;
+            language = lang;
             language1 = l1;
             language2 = l2;
             idx = i;
@@ -150,20 +153,28 @@ void buildGraph(string input){
 
         // Graph - adjacency list
         vector<vector<int>> graph;
+        graph.clear();
 
         // Nodes list
         vector<Node> nodeList;
+        nodeList.clear();
+        int idxCounter = -1; 
 
         // Mapping language string to int (idx)
         map<string,int> lang2idx;
+        lang2idx.clear();
         int langCounter = 0;
 
         ss >> q_words; // Pick the number of words expected
         if (q_words == 0){break; } // Stop if no words are being expected
         ss >> from; // Pick the language we want to begin with
         ss >> to; // Pick te language we want to reach
-        //cout << q_words << " " << from << " " << to << endl;
 
+        // Weights Matrix
+        int weightsMatrix[q_words*2][q_words*2];
+
+        //cout << q_words << " " << from << " " << to << endl;
+        
         // Iterates over the string to grab the words
         for (int i = 0; i < q_words; i++ ){
             for (int j = 0; j < 3; j++){
@@ -182,24 +193,46 @@ void buildGraph(string input){
                 langCounter += 1;
             }            
 
-            Node newNode(l1,l2,p,i);
-            nodeList.push_back(newNode);
+            // Push two, because one word has 2 language associated
+            idxCounter += 1;          
+            Node newNode1(l1,l2,p,idxCounter,l1);
+            idxCounter += 1;  
+            Node newNode2(l1,l2,p,idxCounter,l2);
+            nodeList.push_back(newNode1);
+            nodeList.push_back(newNode2);
+            graph.push_back({-1}); // Put -1 as a neighbor just to initialize
             graph.push_back({-1}); // Put -1 as a neighbor just to initialize
             //graph[nodeList.size()-1].push_back(newNode);
 
+            // Add edge between nodes from the same language
+            graph[graph.size()-2].push_back(newNode2.idx);
+            graph[graph.size()-1].push_back(newNode1.idx);
+
+            // Add wheights to the matrix
+            weightsMatrix[newNode1.idx][newNode2.idx] = 0;
+            weightsMatrix[newNode2.idx][newNode1.idx] = 0;
+            
+            // Add edge to every other with same lang and diff initial letter
             for(unsigned int k = 0; k < nodeList.size(); k++){
-                if (nodeList[k].language1 == l1 || nodeList[k].language1 == l2){
-                    if (nodeList[k].word[0] != newNode.word[0]){ 
-                        int index = newNode.idx;
+                if (nodeList[k].language == l1){
+                    if (nodeList[k].word[0] != newNode1.word[0]){ 
+                        int index = newNode1.idx;
                         graph[k].push_back(index); // Add newNode as neighbor
-                        graph[graph.size()-1].push_back(nodeList[k].idx); // Add neighbor to newNode
+                        weightsMatrix[nodeList[k].idx][index] = nodeList[k].word.length(); // Add weight
+                        
+                        graph[graph.size()-2].push_back(nodeList[k].idx); // Add neighbor to newNode
+                        weightsMatrix[index][nodeList[k].idx] = newNode1.word.length(); // Add weight
+
                     }
                 }
-                if (nodeList[k].language2 == l1 || nodeList[k].language2 == l2){
-                    if (nodeList[k].word[0] != newNode.word[0]){ 
-                        int index = newNode.idx;
+                if (nodeList[k].language == l2){
+                    if (nodeList[k].word[0] != newNode2.word[0]){ 
+                        int index = newNode2.idx;
                         graph[k].push_back(index); // Add newNode as neighbor
+                        weightsMatrix[nodeList[k].idx][index] = nodeList[k].word.length(); // Add weight
+                        
                         graph[graph.size()-1].push_back(nodeList[k].idx); // Add neighbor to newNode
+                        weightsMatrix[index][nodeList[k].idx] = newNode2.word.length(); // Add weight
                     }
                 } 
             }
@@ -211,13 +244,15 @@ void buildGraph(string input){
         }
 
         for (unsigned int i = 0; i < graph.size(); i++){
-            for(unsigned int j = 0; j < graph[i].size(); j++){
-                cout << graph[i][j] << " ";
+            for(unsigned int j = 1; j < graph[i].size(); j++){
+                int vizinho = graph[i][j];
+                int peso = weightsMatrix[i][vizinho];
+                cout << "(" << vizinho << "," << peso << ")" << " ";
             }
             cout << endl;
         }
         for(unsigned int i=0; i<nodeList.size(); i++){
-            cout << nodeList[i].word << endl;
+            cout << nodeList[i].word << " " << nodeList[i].language << endl;
         }
         
         //map<string, int>::iterator it = lang2idx.begin();
