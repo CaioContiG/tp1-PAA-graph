4b28d7eeef33ebd49dfdeb1ab23e5346ea6013dd
unknown <caiocontig@gmail.com>
2024-05-20 20:19:55 -0300

    building map and graph

--- MountRushmore/tentativa.cpp
+++ MountRushmore/tentativa.cpp
@@ -7,6 +7,8 @@ class Node{
         int idx;
         double x;
         double y;
+        char letter;
+        vector<int> neighbors;
 
         // PRIM
         double distance;
@@ -49,15 +51,46 @@ int main() {
     int m, n; // number of translations, of word pairs
     char a, b; // a (letter) translates to b (letter)
     string word1, word2; // word pair
+    vector<Node> graph;
+    map<char,int> word2idx;
 
     cin >> m >> n;
     cout << m << " " << n << endl;
 
+    // Getting translations - Building Graph
+    int cnt = 0;
     for(int i = 0; i < m; i++){
         cin >> a >> b;
+
+        // If word wasn't added yet (maybe it is not needed as map has one key per element?)
+        if(word2idx.find(a) == word2idx.end()) {            
+            word2idx.insert({a,cnt});
+            graph.push_back(Node(cnt));
+            graph[cnt].letter = a;
+            cnt += 1;
+        }        
+        if(word2idx.find(b) == word2idx.end()) {            
+            word2idx.insert({b,cnt});
+            graph.push_back(Node(cnt));
+            graph[cnt].letter = b;
+            cnt += 1;
+        }        
+
+        int idxL1 = word2idx[a];
+        int idxL2 = word2idx[b];
+        graph[idxL1].neighbors.push_back(idxL2);
         cout << a << " " << b << endl;
     }
 
+    cout << "KEY\tELEMENT\n"; 
+    for (auto itr = word2idx.begin(); itr != word2idx.end(); ++itr) { 
+        cout << itr->first 
+             << '\t' << itr->second << '\n'; 
+    }
+cout << "----------" << endl;
+
+
+    // Testing pairs
     for(int i = 0; i < n; i++){
         cin >> word1 >> word2;
         cout << word1 << " " << word2 << endl;
